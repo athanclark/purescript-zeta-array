a641d27ca9ccec52f25bd968067e962b129e38fe
Athan Clark <athan.clark@gmail.com>
2020-07-11 10:07:23 -0400

    broadcast except indicies

--- src/IxZeta/Array.purs
+++ src/IxZeta/Array.purs
@@ -9,7 +9,7 @@ import Effect.Ref (new, read, write) as Ref
 import Zeta.Types (READ, WRITE, kind SCOPE, class SignalScope) as S
 import Queue.Types (READ, WRITE) as Q
 import IxQueue (IxQueue)
-import IxQueue (new, broadcast, on, del) as IxQueue
+import IxQueue (new, broadcast, broadcastExcept, on, del) as IxQueue
 
 
 data ArrayUpdate value
@@ -44,6 +44,12 @@ append x (IxSignalArray {state, queue}) = do
   Ref.write (Array.snoc xs x) state
   IxQueue.broadcast queue (ArrayAppend { index: Array.length xs, valueNew: x })
 
+appendExcept :: forall rw value. Array String -> value -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Unit
+appendExcept indicies x (IxSignalArray {state, queue}) = do
+  xs <- Ref.read state
+  Ref.write (Array.snoc xs x) state
+  IxQueue.broadcastExcept queue indicies (ArrayAppend { index: Array.length xs, valueNew: x })
+
 update :: forall rw value. Int -> (value -> value) -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Boolean
 update index f (IxSignalArray {state, queue}) = do
   xs <- Ref.read state
@@ -54,6 +60,18 @@ update index f (IxSignalArray {state, queue}) = do
       Just xs' -> do
         Ref.write xs' state
         IxQueue.broadcast queue (ArrayUpdate {index, valueOld: x, valueNew: f x})
+        pure true
+
+updateExcept :: forall rw value. Array String -> Int -> (value -> value) -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Boolean
+updateExcept indicies index f (IxSignalArray {state, queue}) = do
+  xs <- Ref.read state
+  case Array.index xs index of
+    Nothing -> pure false
+    Just x -> case Array.modifyAt index f xs of
+      Nothing -> pure false
+      Just xs' -> do
+        Ref.write xs' state
+        IxQueue.broadcastExcept queue indicies (ArrayUpdate {index, valueOld: x, valueNew: f x})
         pure true
 
 delete :: forall rw value. Int -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Boolean
@@ -68,6 +86,18 @@ delete index (IxSignalArray {state, queue}) = do
         IxQueue.broadcast queue (ArrayDelete {index, valueOld: x})
         pure true
 
+deleteExcept :: forall rw value. Array String -> Int -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Boolean
+deleteExcept indicies index (IxSignalArray {state, queue}) = do
+  xs <- Ref.read state
+  case Array.index xs index of
+    Nothing -> pure false
+    Just x -> case Array.deleteAt index xs of
+      Nothing -> pure false
+      Just xs' -> do
+        Ref.write xs' state
+        IxQueue.broadcastExcept queue indicies (ArrayDelete {index, valueOld: x})
+        pure true
+
 move :: forall rw value. Int -> Int -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Boolean
 move indexOld indexNew (IxSignalArray {state, queue}) = do
   xs <- Ref.read state
@@ -80,6 +110,20 @@ move indexOld indexNew (IxSignalArray {state, queue}) = do
         Just xs'' -> do
           Ref.write xs'' state
           IxQueue.broadcast queue (ArrayMove {indexOld, indexNew, value})
+          pure true
+
+moveExcept :: forall rw value. Array String -> Int -> Int -> IxSignalArray (write :: S.WRITE | rw) value -> Effect Boolean
+moveExcept indicies indexOld indexNew (IxSignalArray {state, queue}) = do
+  xs <- Ref.read state
+  case Array.index xs indexOld of
+    Nothing -> pure false
+    Just value -> case Array.deleteAt indexOld xs of
+      Nothing -> pure false
+      Just xs' -> case Array.insertAt indexNew value xs of
+        Nothing -> pure false
+        Just xs'' -> do
+          Ref.write xs'' state
+          IxQueue.broadcastExcept queue indicies (ArrayMove {indexOld, indexNew, value})
           pure true
 
 subscribeLight :: forall rw value. String -> (ArrayUpdate value -> Effect Unit) -> IxSignalArray (read :: S.READ | rw) value -> Effect Unit
